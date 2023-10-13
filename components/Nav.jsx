8dd1363e2553ed08665758fe3456a45f6504f1f5
Dunhill Dimaapi <dunhilldimaapi@Dunhills-Mac-mini.local>
2023-10-13 08:53:22 +0800

    Tutorial done

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,19 +6,19 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
 
             setProviders(response);
         }
 
-        setProviders();
+        setUpProviders();
     }, [])
 
     return (
@@ -35,7 +35,7 @@ const Nav = () => {
             </Link>
 
             <div className='sm:flex hidden'>
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className='flex gap-3 md:gap-5'>
                         <Link href="/create-prompt" className='black_btn'>
                             Create Post
@@ -49,7 +49,7 @@ const Nav = () => {
 
                         <Link href='/profile'>
                             <Image
-                                src='/assets/images/logo.svg'
+                                src={session?.user.image}
                                 width={37}
                                 height={37}
                                 className='rounded-full'
@@ -73,10 +73,10 @@ const Nav = () => {
             </div>
 
             <div className='sm:hidden flex relative'>
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className='flex'>
                         <Image
-                            src='/assets/images/logo.svg'
+                            src={session?.user.image}
                             width={37}
                             height={37}
                             className='rounded-full'
--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
